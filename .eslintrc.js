78c17ade7b818990a4b53af3a94f1ea0fb4a6c95
Yuna <yuna9068@gmail.com>
2023-07-06 17:10:47 +0800

    fix: 處理 ESLint 'no-unused-vars' 問題和修改 'vuejs-accessibility/label-has-for' 規則

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,27 @@ module.exports = {
   },
 
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-pug/vue3-recommended',
     '@vue/airbnb',
   ],
 
+  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@babel/eslint-parser',
+    sourceType: 'module',
   },
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vuejs-accessibility/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
   },
 };
